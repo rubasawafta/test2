78668a45cf8362a5d0d8d5f29ca74c7fc7f90a2d
rubasawafta <31798344+rubasawafta@users.noreply.github.com>
2020-07-03 22:43:20 +0300

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -1,20 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
+import  os
 import shopify
+import re
 
+# API connection
 
-page = requests.get("https://www.nastygal.com/womens/dresses?cat=dresses")
-soup = BeautifulSoup(page.content, 'html.parser')
-con = soup.find('ul', class_='search-result-items')
-res = con.findAll('li', class_='grid-tile')
-
-for result in res:
-      # imag = result.find('div' ,class_='product-image load-bg').text.strip()
-       title =  result.find('div', class_='product-tile-name').text.strip()
-       pric =result.find('span' ,class_='product-standard-price').text.strip()
-       salePrice =result.find('span',class_='product-sales-price' ).text.strip()
-       print(title,pric,salePrice)
-shop_url = "https://%s:%s@fost2a.myshopify.com/admin/api/%s" % (API_KEY, PASSWORD, API_VERSION)
-print(shop_url)
+SHOP_NAME= 'fost2a'
+shop_url = "https://%s:%s@%s.myshopify.com/admin" % (API_KEY, PASSWORD, SHOP_NAME)
 shopify.ShopifyResource.set_site(shop_url)
 shop = shopify.Shop.current()
+
+# connection test
+product = shopify.Product.find(5275133477027)
+print(bool(shopify.Product.find(5275133477027)))
+product.price = 15 ;
+
+
+# scrapping site
+page = requests.get("https://www.nastygal.com/tie-romance-with-me-puff-sleeve-culotte-jumpsuit/AGG47758-2.html?color=165")
+soup = BeautifulSoup(page.content, 'html.parser')
+con = soup.find('div', class_='pdp-main')
+res = con.findAll('div', class_='pdp-main-inner')
+for result in res:
+       title = result.find('h1', class_='product-name').text.strip()
+       pricestandard = result.find('span', class_='price-standard').text.strip()
+       img = result.find('div', class_='product-col-1 product-image-container')
+       pricesales = result.find('span', class_='price-sales').text.strip()
+       size = result.find('div' ,class_='attribute size-attribute').find('ul',class_='swatches size clearfix').text
+     #  print (title,pricestandard, pricesales,size)
+
+
+
+
+# Create a new product
+new_product= shopify.Product()
+new_product.title =title
+new_product.size=size
+new_product.vendor = "fost2a"
+success = new_product.save()
+
+
+
+
