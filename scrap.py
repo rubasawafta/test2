c9a47e9d584015d626c3500dbf0de3a3248c97a6
rubasawafta <31798344+rubasawafta@users.noreply.github.com>
2020-07-05 06:29:15 +0300

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -1,70 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
-import  os
 import shopify
-import re
 
-# API connection
 
+# API connection--------------------------------------------------------------------------------------------------
 API_VERSION = '2020-7'
-SHOP_NAME= 'fost2a'
-shop_url = "https://%s:%s@%s.myshopify.com/admin" % (API_KEY, PASSWORD, SHOP_NAME)
+SHOP_NAME = 'fost2a'
+API_KEY = '5c27637b28787a5217308728c6797a20'
+API_PASSWORD = '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'
+shop_url = "https://%s:%s@%s.myshopify.com/admin" % (API_KEY, API_PASSWORD, SHOP_NAME)
 shopify.ShopifyResource.set_site(shop_url)
 shop = shopify.Shop.current()
 
-
-
-# scrapping site
-page = requests.get("https://www.nastygal.com/tie-romance-with-me-puff-sleeve-culotte-jumpsuit/AGG47758-2.html?color=165")
+# scrapping site---------------------------------------------------------------------------------------------------
+page = requests.get("https://ar-sa.namshi.com/buy-ella-dotted-sheer-sleeve-top-w757055a.html")
 soup = BeautifulSoup(page.content, 'html.parser')
-con = soup.find('div', class_='pdp-main')
-res = con.findAll('div', class_='pdp-main-inner')
+con = soup.find('div', class_='site_width_container')
+res = con.findAll('div', class_='content')
 for result in res:
-       title = result.find('h1', class_='product-name').text.strip()
-       pricestandard = result.find('span', class_='price-standard').text.strip()
-       img = result.find('div', class_='product-col-1 product-image-container')
-       pricesales = result.find('span', class_='price-sales').text.strip()
-       size = result.find('div' ,class_='attribute size-attribute').find('ul',class_='swatches size clearfix').text
-     #  print (title,pricestandard, pricesales,size)
+    # and dont forget to get the SKU of the product it's important
+    # it's a string and you can send it to shopify using
+    # product.sku = "<your scrapped sku>"
+    # and place it for every variant using
+    # variant.sku = "<your scrapped sku>"
+    title = result.find('div',class_='product__options').find('h2', class_='product__brandname').text
+    des=result.find('div',class_='product__options').find('h1',class_='product__name').text
+    print(title,des)
+    images =[]
+    imageContainer = result.findAll('div',class_='slim-slide zoom')
+    for img in imageContainer :
+         r=img.find('img')
+         rt=r.get('src')
+         images.append(rt)
+    print(images)
+    Scrapsku=result.find('table',class_='product_attributes').text.strip('\t *').splitlines()
+    Scrapsku[:] = [item for item in Scrapsku if item != '']
+    print(Scrapsku)
+    sku=Scrapsku[1]
+    print(sku)
+    Model_type=Scrapsku[7]
+    print(Model_type)
+    sizes=result.find('div',class_='size__selector animated').find('ul').text.splitlines()
+    sizes[:] = [item for item in sizes if item != '']
+    print(sizes)
+    pricestandard = str(result.find('p', class_='product__price').find('span').text.strip())
+    print(pricestandard)
 
-product= shopify.Product()
-{
-  "product": {
-    "title": title,
-    "body_html": "<strong>Good snowboard!</strong>",
-    "vendor": "fost2a",
-    "product_type": "Snowboard",
-    "variants": [
-      {
-        "option1": "Blue",
-        "option2": "155"
-      },
-      {
-        "option1": "Black",
-        "option2": "159"
-      }
-    ],
-    "options": [
-      {
-        "name": "Color",
-        "values": [
-          "Blue",
-          "Black"
-        ]
-      },
-      {
-        "name": "Size",
-        "values": [
-          "155",
-          "159"
-        ]
-      }
-    ]
-  }
-}
-
+#add product---------------------------------------------------------------------------------------------------
+product = shopify.Product()
+product.title = title
+product.product_type = Model_type
+product.body_html= des
+product.sku = sku
+variants = []
+for size in sizes:
+    v = shopify.Variant()
+    v.option1 = size
+    v.price = pricestandard
+    v.sku=sku
+    variants.append(v)
+product.variants = variants
+product.options = [
+    {"name": "Size", "values": sizes,"sku":sku}
+]
+product.images = images
 success = product.save()
 
-
-
-
+if product.errors:
+    print(product.errors.full_messages())
